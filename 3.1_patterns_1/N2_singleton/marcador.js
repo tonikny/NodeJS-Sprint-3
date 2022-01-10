5499b8f6cf9c3ef2eca4637b05f4ed8ef74b72e9
tonikny <dalecanya@gmail.com>
2022-01-10 10:26:17 +0100

    - comentari sobre Cached Singleton

--- 3.1_patterns_1/N2_singleton/marcador.js
+++ 3.1_patterns_1/N2_singleton/marcador.js
@@ -1,10 +1,13 @@
 // Cached Singleton
 // https://medium.com/swlh/node-js-and-singleton-pattern-7b08d11c726a
 
+// Degut al sistema de caché dels mòduls NodeJS sempre importarem la mateixa instància del marcador.
+// S'ha de anar en compte de no importar el marcador des d'arxius amb nom diferent perque no funcionaria el Cached Singleton
+
 class Marcador {
   constructor() {
     this.puntuacions = [];
   }
 }
 
-module.exports = new Marcador();
+module.exports = new Marcador();  // s'ha d'exportar l'objecte, no la classe
